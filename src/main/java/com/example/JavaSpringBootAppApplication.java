2eeaeaf6fd5f3982974044f5d4b07d44ae251b1e
pooja4242 <pooja42848@gmail.com>
2024-11-20 18:31:16 +0530

    Initial commit

---
+++ src/main/java/com/example/JavaSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootAppApplication.class, args);
+	}
+
+}
